66d1c57737a40b15e84a5a40238b6bd9168712ac
DESKTOP-5RTJJSJ\tejug <tejugowda14@gmail.com>
2021-04-24 18:48:57 +0530

    create card component for note

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
- <div id="app">
+<div id="app">
   <Navbar @openModal="modal=true"/> 
   <div class="modal" v-if="modal">
+    <form @submit.prevent="submitNote">
     <div class="title-wrap">
-        <div class="title">Enter title</div>
-        <div class="close-btn" @click.self="handleNoteAddLocal(note_title, note_data)"> 👍 </div>
-      </div>
+      <input class="title" placeholder="Enter title" v-model="note.title" required />
+      <button class="close-btn" type="submit" > 👍 </button>
+      <div class="close-btn" @click="closebtn">⛔</div>
+    </div>
+    <textarea placeholder="Enter your note details" v-model="note.detail" required/>
+    </form>
   </div>
+  <div class="card-wrap"  v-if="savedNotes.length > 0" >
+    <div class="cards" v-for='savedNote in savedNotes' :key="savedNote" >
+      <p class="title">{{savedNote.title}}</p>
+      <p class="detail">{{savedNote.detail}}</p>
+    </div>
   </div>
+  <div class="empty-state" v-else>
+    <p>No notes available.<br> Please create new note</p>
+    </div>
+</div>
 </template>
 
 <script>
@@ -22,16 +35,25 @@ export default {
   data(){
     return{
         modal:false,
-        note_title: "",
-        note_data: "",
+        savedNotes:[],
+        note:{
+          title:'',
+          detail:''
+        }
     }
   },
     methods:{
-    handleNoteAddLocal(note_title, note_data) {
-      this.note_title = "";
-      this.note_data = "";
-      this.handleNoteAdd(note_title, note_data);
-    },
+    closebtn(){
+        this.modal = false; 
+      },
+
+    submitNote(){
+      let tempNote = {...this.note};
+      this.savedNotes.push(tempNote);
+      this.note.title='';
+      this.note.detail='';
+      this.modal = false; 
+    }
   }
 };
 </script>
@@ -46,7 +68,7 @@ body{
 .modal{
   width: 40%;
   height: 50vh;
-  background-color: #161616;;
+  background-color: #161616;
   position: absolute;
   top: 30%;
   left: 30%;
@@ -62,16 +84,79 @@ body{
     .title{
       font-size: 24px;
       font-weight: 700;
-    
       padding: 8px 10px;
+      background-color: #0ff;
+      border: none;
+      outline: none;
     }
 
     .close-btn{
       padding: 8px 10px 8px 10px;
       font-size: 24px;
       font-weight: 700;
-      
+      cursor: pointer;
+      background-color: #0ff;
+      border: none;
+      outline: none;
+    }
+  }
+
+  textarea{
+    color: #fff;
+    background-color: #161616;
+    border: none;
+    width: 100%;
+    height: 100%;
+    padding: 5px 5px;
+    
+    &::-webkit-scrollbar{
+      display: none;
     }
   }
 }
+
+.card-wrap{
+  padding: 100px 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  
+  .cards{
+  width: 200px;
+  height: 200px;
+  background-color: #8EE4AF;;
+  cursor: pointer;
+  padding:16px;
+  margin-bottom: 20px;
+
+    &:hover{
+      transform: scale(1.1);
+      box-shadow: 1px 1px 10px  #8EE4AF;
+    }
+
+  .title{
+    font-size: 16px;
+    font-weight: 700;
+    margin: 0 0 20px 0;
+  }
+
+  .detail{
+    font-size: 12px;
+    font-weight: 500;
+    margin: unset;
+  }
+}
+}
+.empty-state{
+  height: 50vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #161616;
+  font-size: 24px;
+  font-weight: 700;
+}
+
 </style>
