2766d8b63a335831538551a9551d72bad3e0382e
DESKTOP-5RTJJSJ\tejug <tejugowda14@gmail.com>
2021-04-24 09:43:02 +0530

    developed outline

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+ <div id="app">
+  <Navbar @openModal="modal=true"/> 
+  <div class="modal" v-if="modal">
+    <div class="title-wrap">
+        <div class="title">Enter title</div>
+        <div class="close-btn" @click.self="handleNoteAddLocal(note_title, note_data)"> 👍 </div>
+      </div>
+  </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Navbar
   },
+
+  data(){
+    return{
+        modal:false,
+        note_title: "",
+        note_data: "",
+    }
+  },
+    methods:{
+    handleNoteAddLocal(note_title, note_data) {
+      this.note_title = "";
+      this.note_data = "";
+      this.handleNoteAdd(note_title, note_data);
+    },
+  }
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+
+body{
+  margin: 0px;
+  padding: 0px;
+}
+
+.modal{
+  width: 40%;
+  height: 50vh;
+  background-color: #161616;;
+  position: absolute;
+  top: 30%;
+  left: 30%;
+
+  .title-wrap{
+    background-color: #0ff;
+    height: 5vh;
+    display: flex;
+    padding: 10px 10px;
+    justify-content: center;
+    align-items: center;
+
+    .title{
+      font-size: 24px;
+      font-weight: 700;
+    
+      padding: 8px 10px;
+    }
+
+    .close-btn{
+      padding: 8px 10px 8px 10px;
+      font-size: 24px;
+      font-weight: 700;
+      
+    }
+  }
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="Navbar">
+        <div class="navbar-logo">Notes App</div>
+        <input class="searc-bar" type="search" placeholder="search"/>
+        <div class="create-btn" @click="openModal"><span>📋</span></div>
+        <div class="customselect-btn"> ⇅ </div>
+        <div class="logout-btn"> 👋 </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Navbar",
+        methods:{
+            openModal(){
+                this.$emit('openModal')
+            }
+        }
+    }
+</script>
+
+<style lang='scss' scoped>
+
+.Navbar{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px 0px;
+    width: 100%;
+    position: fixed;
+    background-color: #111;
+    align-items: center;
+    margin: 0px;
+
+    .navbar-logo{
+        font-size: 1.6em;
+        cursor: pointer;
+        color: #00a3a3;
+        font-family: RocknRoll One,sans-serif;
+    }
+
+    .searc-bar{
+        width: 25%;
+        padding: 10px 0px;
+        border: 3px solid #c4cdd1;
+        outline: none;
+        border-radius: 10px;
+        color: #000;
+        font-size: 1em;
+        font-weight: 500;
+        background-color: #c4cdd1;
+        text-align: center;
+    }
+
+    .create-btn{
+        padding: 5px;
+        border-radius: 25px;
+        color: #000;
+        background-color: #0ff;
+        font-size: 1.6em;
+        font-weight: 500;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        &:hover{
+            cursor: pointer;
+            background-color: #00a3a3;
+            box-shadow: 1px 1px 10px #00a3a3;
+        }
+    }
+
+    .customselect-btn{
+        border-radius: 25px;
+        color: #000;
+        background-color: #0ff;
+        font-size: 1.6em;
+        font-weight: 500;
+        display: flex;
+        align-items: center;
+        padding: 5px;
+        justify-content: center;
+
+            &:hover{
+                cursor: pointer;
+                background-color: #00a3a3;
+                box-shadow: 1px 1px 10px #00a3a3;
+            }
+    }
+
+    .logout-btn{
+        border-radius: 25px;
+        color: #000;
+        background-color: #0ff;
+        font-size: 1.6em;
+        font-weight: 500;
+        display: flex;
+        align-items: center;
+        padding: 5px;
+        justify-content: center;
+
+            &:hover{
+                cursor: pointer;
+                background-color: #00a3a3;
+                box-shadow: 1px 1px 10px #00a3a3;
+            }
+    }
+}
+</style>
